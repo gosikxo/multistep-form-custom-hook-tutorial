179749f07bdd4808f81f7858be058e08fd5e6dcf
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-01-06 16:47:47 +0100

    Add Form State and Handle Form Submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,49 @@
-import { FormEvent } from "react"
+import { FormEvent, useState } from "react"
 import { AccountForm } from "./AccountForm"
 import { AddressForm } from "./AddressForm"
 import { useMultistepForm } from "./useMultistepForm"
 import { UserForm } from "./UserForm"
 
+type FormData = {
+  firstName: string
+  lastName: string
+  age: string
+  street: string
+  city: string
+  state: string
+  zip: string
+  email: string
+  password: string
+}
+
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: ""
+}
+
 function App() {
+  const [data, setData] = useState(INITIAL_DATA)
+  function updateFields(fields: Partial<FormData>) {
+    setData(prev => {
+      return { ...prev, ...fields }
+    })
+  }
   const { steps, currentStepIndex, step, isFirstStep, back, next, isLastStep } = useMultistepForm([
-    <UserForm />,
-    <AddressForm />,
-    <AccountForm />
+    <UserForm {...data} updateFields={updateFields} />,
+    <AddressForm {...data} updateFields={updateFields} />,
+    <AccountForm {...data} updateFields={updateFields} />
   ])
   function onSubmit(e: FormEvent) {
     e.preventDefault()
-    next()
+    if (!isLastStep) return next()
+    alert("Successful Account Creation")
   }
   return (
     <div style={{
@@ -22,7 +53,8 @@ function App() {
       padding: "2rem",
       margin: "1rem",
       borderRadius: ".5rem",
-      fontFamily: "Arial"
+      fontFamily: "Arial",
+      maxWidth: "max-content"
     }}>
       <form onSubmit={onSubmit}>
         <div style={{
